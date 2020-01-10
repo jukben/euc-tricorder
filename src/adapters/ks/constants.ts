0775fa61d884941c93060a394dfb370e07b19f17
Jakub Benes <jukben@gmail.com>
2020-01-10 22:29:02 +0100

    wip

---
+++ src/adapters/ks/constants.ts
@@ -0,0 +1,27 @@
+export const KS_SERVICE = '0000ffe0-0000-1000-8000-00805f9b34fb';
+export const KS_CHAR = '0000ffe1-0000-1000-8000-00805f9b34fb';
+
+export const KS_COMMANDS = {
+  REQUEST_NAME: new Int8Array([
+    -86,
+    85,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    -101,
+    20,
+    90,
+    90,
+  ]),
+} as const;
---
+++ src/adapters/ks/index.ts
@@ -0,0 +1,1 @@
+export * from './ks18xl';
