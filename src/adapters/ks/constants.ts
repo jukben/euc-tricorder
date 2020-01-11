0fb2e70ad9f039e16714bccff8796a5b3e10912f
Jakub Benes <jukben@gmail.com>
2020-01-11 16:16:27 +0100

    wip

--- src/adapters/ks/constants.ts
+++ src/adapters/ks/constants.ts
@@ -1,27 +1,31 @@
+import { Buffer } from 'buffer';
+
 export const KS_SERVICE = '0000ffe0-0000-1000-8000-00805f9b34fb';
 export const KS_CHAR = '0000ffe1-0000-1000-8000-00805f9b34fb';
 
 export const KS_COMMANDS = {
-  REQUEST_NAME: new Int8Array([
-    -86,
-    85,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    -101,
-    20,
-    90,
-    90,
-  ]),
+  REQUEST_NAME: Buffer.from(
+    new Int8Array([
+      -86,
+      85,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      -101,
+      20,
+      90,
+      90,
+    ]),
+  ).toString('base64'),
 } as const;
