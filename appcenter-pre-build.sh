2b2235b7f50234aea93938c7367213ad6753c5d9
Jakub Benes <jukben@gmail.com>
2020-03-07 16:02:49 +0100

    feat: create env file for production from CI varible

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,3 +4,6 @@ echo "Injecting secrets..."
 
 echo "Updating AppCenter Config"
 echo $APPCENTER_CONFIG | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/AppCenter-Config.plist"
+
+echo "Updating AppCenter Config"
+echo $DOTENV | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/.env.production"
