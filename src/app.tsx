5c82065669eec18f5cd48b77c94e6c25f031dd05
Jakub Beneš <jukben@gmail.com>
2020-04-18 13:47:23 +0200

    RN Upgrade + small fix (#14)
    * chore: upgrade to React Native 0.62
    
    * fix: useDeviceScan dependencies
    
    * refactor: move to react-native-testing-library

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import { AdapterProvider, BleProvider } from '@euc-tricorder/providers';
 import { SettingsProvider } from '@euc-tricorder/providers/settings';
-import { NavigationNativeContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 
 import { Crossroad } from './routes/crossroad';
 
 export const App = () => (
-  <NavigationNativeContainer>
+  <NavigationContainer>
     <SettingsProvider>
       <BleProvider>
         <AdapterProvider>
@@ -14,5 +14,5 @@ export const App = () => (
         </AdapterProvider>
       </BleProvider>
     </SettingsProvider>
-  </NavigationNativeContainer>
+  </NavigationContainer>
 );
