8507c87f878d6846ba86054a88e96d1de0082437
Jakub Benes <jukben@gmail.com>
2020-07-18 20:32:12 +0200

    ci: fixed appcenter-pre-build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,9 +5,9 @@ echo "Injecting secrets..."
 echo "Updating AppCenter Config"
 echo $APPCENTER_CONFIG | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/AppCenter-Config.plist"
 echo "File AppCenter-Config.plist has been created sucessfuly!"
-cat "$APPCENTER_SOURCE_DIRECTORY/.env"
+cat "$APPCENTER_SOURCE_DIRECTORY/ios/AppCenter-Config.plist"
 
-echo '/n/n';
+printf '\n';
 
 echo "Updating .env file..."
 echo $DOTENV | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/.env"
