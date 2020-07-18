e268f278ac71ee7afd43eabcceba8621f89cc6af
Jakub Beneš <jukben@gmail.com>
2020-07-18 19:16:33 +0200

    Fix generation .env file on CD (#20)

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,5 +5,5 @@ echo "Injecting secrets..."
 echo "Updating AppCenter Config"
 echo $APPCENTER_CONFIG | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/AppCenter-Config.plist"
 
-echo "Updating .env.production file"
-echo $DOTENV | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/.env.production"
+echo "Updating .env file"
+echo $DOTENV | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/.env"
