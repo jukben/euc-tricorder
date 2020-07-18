f1ffc8b3fa626ed16626caf38839905f04f798a8
Jakub Benes <jukben@gmail.com>
2020-07-18 20:22:19 +0200

    ci: show generated files for better debug

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,6 +4,12 @@ echo "Injecting secrets..."
 
 echo "Updating AppCenter Config"
 echo $APPCENTER_CONFIG | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/AppCenter-Config.plist"
+echo "File AppCenter-Config.plist has been created sucessfuly!"
+cat "$APPCENTER_SOURCE_DIRECTORY/.env"
 
-echo "Updating .env file"
+echo '/n/n';
+
+echo "Updating .env file..."
 echo $DOTENV | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/.env"
+echo "File .env has been created sucessfuly!"
+cat "$APPCENTER_SOURCE_DIRECTORY/.env"
